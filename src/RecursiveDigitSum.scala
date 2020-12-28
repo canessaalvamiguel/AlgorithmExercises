c92e673c0e56242c627b68db3bbd530845fda66f
miguelcanessa <canessaalvamiguel@gmail.com>
2020-12-27 22:58:21 -0500

    Solve RecursiveDigitSum

---
+++ src/RecursiveDigitSum.scala
@@ -0,0 +1,23 @@
+/*
+* Link to problem: https://www.hackerrank.com/challenges/recursive-digit-sum/problem
+* */
+object RecursiveDigitSum {
+
+  // Complete the superDigit function below.
+  def superDigit(n: String, k: Int): Int = {
+    solution( (solution(n) * k).toString ) ;
+  }
+
+  def solution(x: String): Int = {
+    if(x.length == 1) x.toInt
+    else{
+      val sum:Long = x.map{ _.asDigit }.foldLeft(0){ _+_ }
+      solution(sum.toString)
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(superDigit("148",3))
+    println(superDigit("148",7))
+  }
+}
