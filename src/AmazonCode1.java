c95096d59e3d4cf15169bddbde328d407acf6c96
miguelcanessa <canessaalvamiguel@gmail.com>
2020-12-29 18:49:23 -0500

    Add amazon exercises solutions

---
+++ src/AmazonCode1.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class AmazonCode1 {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(new String[]{"2abc", "bcd", "cab"},new String[]{"dbc", "2abc", "cab", "bcd", "bcb"})));
+        System.out.println(Arrays.toString(solution(new String[]{"in", "the", "spain"},new String[]{"the", "spain", "that", "the", "rain", "in", "spain", "stays", "forecast", "in", "the"})));
+    }
+
+    public static int[] solution(String[] target, String[] source){
+
+        Boolean continue_loop     = true;
+        int target_length         = target.length;
+        int index                 = 0;
+        int[] result_indexes      = new int[2];
+
+        while( continue_loop ){
+            for( int i = index; i < source.length - target_length; i++){
+                String[] subArray = subArray( source, i, i + target_length );
+                if( arrayContainsTarget(subArray, target) ){
+                    result_indexes[0] = i;
+                    result_indexes[1] = i + target_length - 1;
+                    continue_loop = false;
+                }
+            }
+            index++;
+            target_length++;
+        }
+
+        return result_indexes;
+
+    }
+
+    public static Boolean arrayContainsTarget(String[] source, String[] target){
+        for( String element : target){
+            if( !Arrays.asList(source).contains(element)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static String[] subArray(String[] array, int beg, int end) {
+        return Arrays.copyOfRange(array, beg, end );
+    }
+
+}
---
+++ src/AmazonCode2.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class AmazonCode2 {
+    public static void main(String[] args) {
+        System.out.println(solution("qwerccw23",3));
+        System.out.println(solution("inthespainn",4));
+    }
+
+    public static List<String> solution(String word, int k){
+
+        int target_length   = k;
+        List results        = new ArrayList<String>();
+
+        for( int i = 0; i < word.length() - target_length + 1; i++){
+            String subString = word.substring(i, i + target_length);
+            if( onlyOneCharacterIsRepeated(subString) ){
+                results.add(subString);
+            }
+        }
+        return results;
+
+    }
+
+    public static Boolean onlyOneCharacterIsRepeated(String source){
+
+        Map<Character, Integer> characters = new HashMap<>();
+
+        for (int i = 0; i < source.length(); i++){
+            Character key = source.charAt(i);
+            if( characters.containsKey(key)){
+                characters.put(key, characters.get(key) + 1);
+            }else{
+                characters.put(key,1);
+            }
+        }
+
+        Boolean hasOneCharacterRepeated = false;
+        for (Map.Entry<Character, Integer> entry : characters.entrySet()) {
+            Integer counter = Integer.valueOf(entry.getValue().toString());
+            if( counter == 2 && hasOneCharacterRepeated == false){
+                hasOneCharacterRepeated = true;
+            }else if( counter == 2 && hasOneCharacterRepeated == true){
+                return false;
+            }else if( counter > 1){
+                return false;
+            }
+        }
+
+        return hasOneCharacterRepeated;
+    }
+
+}
